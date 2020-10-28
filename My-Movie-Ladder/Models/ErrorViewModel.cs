3350114dadb6434ceaa89ce653f719df9a9d30b2
owhiteman <ollie.whiteman@gmail.com>
2020-10-28 23:18:14 +1300

    * .gitignore: Initial commit. First API call working
    * My-Movie-Ladder.sln:
    * app.db:
    * Startup.cs:
    * Program.cs:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * My-Movie-Ladder.csproj:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Privacy.cshtml:
    * Error.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * ApplicationDbContext.cs:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * launchSettings.json:
    * LICENSE.txt:
    * _LoginPartial.cshtml:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * _ViewStart.cshtml:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap.bundle.js:
    * bootstrap-grid.css:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * ApplicationDbContextModelSnapshot.cs:
    * jquery.validate.js:
    * LICENSE.txt:
    * bootstrap-grid.min.css:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.css.map:
    * bootstrap.bundle.min.js:
    * 00000000000000_CreateIdentitySchema.cs:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * bootstrap-grid.min.css.map:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * 00000000000000_CreateIdentitySchema.Designer.cs:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ My-Movie-Ladder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace My_Movie_Ladder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
