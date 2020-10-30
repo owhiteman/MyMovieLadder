82e6c1eb790f08d1ee01333114398ac2fa0a6d36
owhiteman <ollie.whiteman@gmail.com>
2020-10-30 15:28:06 +1300

    Now displaying popular movies on the home page.

--- My-Movie-Ladder/GetRatedMovies.cs
+++ My-Movie-Ladder/GetRatedMovies.cs
@@ -62,15 +62,47 @@ namespace My_Movie_Ladder
         }
 
 
+    }
 
+    public class Movies
+    {
+        public int page { get; set; }
+        public int total_results { get; set; }
+        public int total_pages { get; set; }
+        public IList<Result> results { get; set; }
+
+        public class Result
+        {
+            public double popularity { get; set; }
+            public int vote_count { get; set; }
+            public bool video { get; set; }
+            public string poster_path { get; set; }
+            public int id { get; set; }
+            public bool adult { get; set; }
+            public string backdrop_path { get; set; }
+            public string original_language { get; set; }
+            public string original_title { get; set; }
+            public IList<int> genre_ids { get; set; }
+            public string title { get; set; }
+            public double vote_average { get; set; }
+            public string overview { get; set; }
+            public string release_date { get; set; }
+        }
+
+
+            
+        
     }
 
     public class GetRatedMovies
     {
         private const string URL = "https://api.themoviedb.org/3/";
         private string urlParameters = "movie/278?api_key=663a0dd265290b5f684214244f6e7f0b&language=en-US";
+        private string urlParam = "discover/movie?api_key=663a0dd265290b5f684214244f6e7f0b&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=3000";
 
-        public String GetMovie()
+        Movies movie = null;
+
+        public void GetMovie()
         {
             HttpClient client = new HttpClient();
 
@@ -80,20 +112,38 @@ namespace My_Movie_Ladder
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
 
-            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
-            Movie movie = null;
+            HttpResponseMessage response = client.GetAsync(urlParam).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            
             if (response.IsSuccessStatusCode)
             {
                 var result = response.Content.ReadAsStringAsync().Result;
-                movie = JsonConvert.DeserializeObject<Movie>(result);
-                return movie.title;
+                movie = JsonConvert.DeserializeObject<Movies>(result);
+                
             }
             else
             {
-                return "Zudis";
+                Console.WriteLine("Zudis");
             }
-
         }
 
+        public string MovieTitle(int x)
+        {
+            return movie.results[x].title;
+        }
+
+        public string MovieReleaseDate(int x)
+        {
+            return movie.results[x].release_date;
+        }
+
+        public double MovieRating(int x)
+        {
+            return movie.results[x].vote_average;
+        }
+
+        public string PosterPath(int x)
+        {
+            return "https://image.tmdb.org/t/p/w200" + movie.results[x].poster_path;
+        }
     }
 }
